f9d69d86d7118590648ec18631adf8a8a90bb3ef
SzymonSiwek <szymek.siwek@gmail.com>
2023-05-21 14:38:09 +0200

    Gotowy program do obliczania

--- Zadanie5/Program.cs
+++ Zadanie5/Program.cs
@@ -1,14 +1,35 @@
 ﻿using System;
 
 namespace RównanieKwadratowe
-{   
-        public class RownanieKwadratowe
+{
+    public class RownanieKwadratowe
     {
-        public static void ObliczPierwiastki(double a, double b, double c)
+        public static (double x1, double x2) ObliczPierwiastki(double a, double b, double c)
         {
             double delta = b * b - 4 * a * c;
+
+            if (delta < 0)
+            {
+                // Brak pierwiastków rzeczywistych
+                return (double.NaN, double.NaN);
+            }
+            else if (delta == 0)
+            {
+                // Jeden pierwiastek rzeczywisty
+                double x = -b / (2 * a);
+                Console.WriteLine("Jeden pierwiastek rzeczywisty");
+                return (x, double.NaN);
+            }
+            else
+            {
+                // Dwa pierwiastki rzeczywiste
+                double x1 = (-b - Math.Sqrt(delta)) / (2 * a);
+                double x2 = (-b + Math.Sqrt(delta)) / (2 * a);
+                return (x1, x2);
+            }
         }
     }
+
     public class Program
     {
         public static void Main(string[] args)
@@ -24,7 +45,11 @@ namespace RównanieKwadratowe
             Console.Write("c: ");
             double c = Convert.ToDouble(Console.ReadLine());
 
+            var (x1, x2) = RownanieKwadratowe.ObliczPierwiastki(a, b, c);
+
             Console.WriteLine("Pierwiastki równania kwadratowego:");
+            Console.WriteLine($"x1 = {x1}");
+            Console.WriteLine($"x2 = {x2}");
 
             Console.ReadKey();
         }

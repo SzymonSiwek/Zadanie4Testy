d828c5bcfed3c4ed24bfb6d0082da55c6cccb03a
SzymonSiwek <szymek.siwek@gmail.com>
2023-05-21 14:29:52 +0200

    Utworzenie klasy program do pobrania danych

---
+++ Zadanie5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace RównanieKwadratowe
+{   
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj współczynniki równania kwadratowego ax^2 + bx + c = 0");
+
+            Console.Write("a: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("b: ");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("c: ");
+            double c = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Pierwiastki równania kwadratowego:");
+
+            Console.ReadKey();
+        }
+    }
+}
